9683a4f2c2ebb74e8640e4d42d98a829fdbfcad3
doraemon-kub <natthawut.sag@spumail.net>
2026-09-13 11:35:40 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CSI402_Project_final.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
